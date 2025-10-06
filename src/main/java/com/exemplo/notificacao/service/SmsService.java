615136adf6b04d6331bd836e6dee5388ace842d8
Vinicinsss <143666494+Vinicinsss@users.noreply.github.com>
2025-10-06 22:27:26 +0000

    Criação e alteração de alguns arquivos

--- src/main/java/com/exemplo/notificacao/service/SmsService.java
+++ src/main/java/com/exemplo/notificacao/service/SmsService.java
@@ -4,7 +4,8 @@ import com.exemplo.notificacao.model.Pedido;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SmsService {
+public class SmsService implements Notificador {
+    @Override
     public void enviar(Pedido pedido) {
         System.out.println("Enviando SMS para " + pedido.getCliente());
     }
